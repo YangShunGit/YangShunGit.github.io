56f6350fc7501299962e2622a25a69fb39b5b9da
yangshun <1197593461@qq.com>
2025-06-20 10:18:29 +0800

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: '个人知识库',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -26,7 +26,6 @@ const config: Config = {
   projectName: 'YangShunGit.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages', // 部署分支
   trailingSlash: false,
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
